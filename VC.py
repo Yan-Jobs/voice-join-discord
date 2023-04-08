fae94f0f4149a4bfd905288efac2088767ed846e
xL3gen <126000086+xL3gen@users.noreply.github.com>
2023-04-08 14:00:07 +0300

    Add files via upload

---
+++ VC.py
@@ -0,0 +1,34 @@
+from json import loads
+from time import sleep
+from json import dumps
+from websocket import WebSocket
+from concurrent.futures import ThreadPoolExecutor
+count = 0
+while(count < 999999):
+    count = count + 1
+    guild_id = 1087038894358409226
+    chid = 1094022662101139486
+    tokenlist = open("tokens.txt").read().splitlines()
+    executor = ThreadPoolExecutor(max_workers=int(1000000))
+    mute = False
+    deaf = False
+    def run(token) :
+        ws = WebSocket()
+        ws.connect("wss://gateway.discord.gg/?v=9&encoding=json")
+        hello = loads(ws.recv())
+        heartbeat_interval = hello['d']['heartbeat_interval']
+        ws.send(dumps({"op": 2,"d": {"token": token,"properties": {"$os": "windows","$browser": "Discord","$device": "desktop"}}}))
+        ws.send(dumps({"op": 4,"d": {"guild_id": guild_id,"channel_id": chid,"self_mute": mute,"self_deaf": deaf}}))
+        ws.send(dumps({"op": 18,"d": {"type": "guild","guild_id": guild_id,"channel_id": chid,"preferred_region": "singapore"}}))
+        while True:
+            sleep(heartbeat_interval/1000)
+            try:
+                ws.send(dumps({"op": 1,"d": None}))
+            except Exception:
+                break
+
+    i = 0
+    for token in tokenlist:
+        executor.submit(run, token)
+        i+=1
+        print("WebSocket Connected : {}".format(i))
